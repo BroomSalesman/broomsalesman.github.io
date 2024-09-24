196172142dc57722d2b330f7cea85012b09613e6
Labeeb <farooqi.labeeb07@gmail.com>
2024-09-24 11:05:15 -0600

    changes to traffic light

--- traffic-light/sketch.js
+++ traffic-light/sketch.js
@@ -2,29 +2,52 @@
 // September 24, 2024
 
 //starts at red so green is  first color shown
-const colors = [ 'red', 'green', 'yellow'];
-let waitTime = 1000; // change to 5000 once complete
-let colorIndice = 0;
+const lightStates = [['white', 'white','red'], ['green', 'white', 'white' ], ['white', 'yellow', 'white']];
+let sequenceIndice = 0;
+let timer;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let timer = 1000; //switch to 5000 after done
 }
 
 function draw() {
   background(220);
+  trafficLights();
+  drawOutLineOfLights();
 }
 
-function
+
+function drawOutLineOfLights() {
+  //box
+  rectMode(CENTER);
+  fill(0);
+  rect(width/2, height/2, 75, 200, 10);
+
+  //lights
+
+  fill(lightStates[sequenceIndice][0]);
+  ellipse(width/2, height/2 - 65, 50, 50);
+
+  fill(lightStates[sequenceIndice][1]);
+  ellipse(width/2, height/2, 50, 50);
+
+  fill(lightStates[sequenceIndice][2]);
+  ellipse(width/2, height/2 + 65, 50, 50);
+}
+
 
 function trafficLights() {
-  colorIndice += 1;
 
-  if (colorIndice >= 3) {
-    colorIndice = 0;
+  if (millis() > timer) {
+    sequenceIndice += 1;
+    console.log('dont wait')
+  }
+  else {
+    console.log('wait');
   }
 
-
-
-
-
+  if (sequenceIndice >= 3) {
+    sequenceIndice = 0;
+}
 }
