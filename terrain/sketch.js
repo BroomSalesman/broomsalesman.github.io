3a54afbb2e3e6162c7670987692e898c56597890
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2024-10-07 11:04:11 -0600

    changes

--- terrain/sketch.js
+++ terrain/sketch.js
@@ -1,15 +1,43 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Terrain Generation
+// October 7, 2024
 
+let terrain = [];
+const NUMBER_OF_RECTS = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let widthOfRect = width/NUMBER_OF_RECTS;
+  generateTerrain(widthOfRect);
 }
 
 function draw() {
   background(220);
+
+  for (let someRect of terrain) {
+    rect(someRect.x, someRect.y, someRect.w, someRect.h);
+  }
+}
+
+
+function generateTerrain(widthOfRect) {
+  let time = 0;
+  let deltaTime = 0.001;
+  for (let x = 0; x < width; x += widthOfRect) {
+    let theHeight = noise(time) * height;
+    let someRect = spawnRectangle(x, theHeight, widthOfRect);
+    terrain.push(someRect);
+    time += deltaTime;
+  }
+}
+
+
+function spawnRectangle(leftSide, rectHeight, rectWidth) {
+  let theRect = {
+    x: leftSide,
+    y: height - rectHeight,
+    w: rectWidth,
+    h: rectHeight,
+  };
+
+  return theRect;
 }
