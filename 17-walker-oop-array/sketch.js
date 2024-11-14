84604d30918f3ee24e56329689efae5469b95f15
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2024-11-14 10:27:46 -0600

    new demo

---
+++ 17-walker-oop-array/sketch.js
@@ -0,0 +1,62 @@
+// Walker OOP Demo
+// November 12, 2024
+
+class Walker {
+  constructor(x, y, theColor) {
+    this.x = x;
+    this.y = y;
+    this.speed = 15;
+    this.radius = 5;
+    this.color = theColor;
+  }
+
+  display() {
+    noStroke();
+    fill(this.color);
+    circle(this.x, this.y, this.radius*2);
+  }
+
+  move() {
+    let choice = random(100);
+    if (choice < 25) {
+      //up
+      this.y -= this.speed;
+    }
+    else if (choice < 50){
+      //downn
+      this.y += this.speed;
+    }
+    else if (choice < 75) {
+      //left
+      this.x -= this.speed;
+    }
+    else {
+      //right
+      this.x += this.speed;
+    }
+  }
+}
+
+let walkerArray = [];
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  winston = new Walker(width/2, height/2, 'red');
+  walkerArray.push(winston);
+
+
+}
+
+function draw() {
+  for (let theWalker of walkerArray) {
+    theWalker.move()
+    theWalker.display();
+  }
+}
+
+function mousePressed() {
+  let randomColor = color(random(255), random(255), random(255));
+  let someWalker = new Walker(mouseX, mouseY, randomColor);
+  walkerArray.push(someWalker);
+}
