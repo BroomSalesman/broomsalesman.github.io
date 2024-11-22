e073642c40f8a15cd4c3a50e77571dfe9f1b666d
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2024-11-22 11:03:04 -0600

    matter.js intro  and collide2d demo

--- matterjs/intro/sketch.js
+++ matterjs/intro/sketch.js
@@ -2,16 +2,36 @@
 // Labeeb Farooqi
 // November 21, 2024
 
-let Engine = Matter.Engine,
-  //Render = Matter.Render,
-  World = Matter.World,
-  Bodies = Matter.Bodies
+const {Engine, Body, Bodies, Composite, Render, Runner} = Matter;
+
+let engine;
+let render;
+let runner;
+let box;
+let ground;
 
 function setup() {
-  var engine = Engine.create();
-  createCanvas(windowWidth, windowHeight);
+  noCanvas();
+  engine = Engine.create();
+  render = Render.create( {
+    element: document.body,
+    engine: engine,
+    options: {
+      width: windowWidth,
+      height: windowHeight
+    }
+  });
+
+  box = Bodies.rectangle(100, 100, 50, 50);
+  ground = Bodies.rectangle(windowWidth/2 , windowHeight - 100, windowWidth, 10, {isStatic: true});
+
+  Composite.add(engine.world, [box, ground]);
+
+  Render.run(render);
+
+  runner = Runner.create();
+
+  Runner.run(runner, engine);
 }
 
-function draw() {
-  background(220);
-}
+
