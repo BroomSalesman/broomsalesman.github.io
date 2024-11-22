a19be3ae53a0b7109d53e7860d2ebb75ee3845a5
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2024-11-22 09:59:50 -0600

    changes + matter.js demos started

---
+++ matterjs/intro/sketch.js
@@ -0,0 +1,17 @@
+// Intro Matter.js
+// Labeeb Farooqi
+// November 21, 2024
+
+let Engine = Matter.Engine,
+  //Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies
+
+function setup() {
+  var engine = Engine.create();
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
